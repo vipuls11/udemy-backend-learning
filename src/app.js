977a268a0bc148681fbeb6ecfcb556a14e11a274
vipuls11 <vipulvishwakrma161@gmail.com>
2026-04-20 12:49:12 +0530

    change in src

--- src/app.js
+++ src/app.js
@@ -11,12 +11,8 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-// app.use(cors({
-//   origin: "http://localhost:3000",
-//   credentials: true,
-// }));
 app.use(cors({
-  origin: "https://udemy-learning-project-react-mern.vercel.app",
+  origin: process.env.CLIENT_URL,
   credentials: true,
 }));
 app.use(cookieParser());
