fb9c55cc0049ee4cbf193d84488bb39e56884075
vipuls11 <vipulvishwakrma161@gmail.com>
2026-04-21 22:37:18 +0530

    change in app.js

--- src/app.js
+++ src/app.js
@@ -11,6 +11,10 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(express.json({ limit: '10mb' }));
+// app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors({
   origin: process.env.CLIENT_URL,
   credentials: true,
